18e5be0c5255ada6d30457557a61fb43055f1506
harish Karwasara <pemaharish@gmail.com>
2025-01-23 22:45:53 +0530

    first

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -4,8 +4,8 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import { useState } from 'react';
 import Alert from './components/Alert';
-// import About from './components/About';
-
+import About from './components/About';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
     const [mode, setMode] = useState('light');
@@ -37,14 +37,20 @@ function App() {
 
   return (
       <>
-      
-     <Navbar title="textutils" aboutText="about textutil33" mode={mode} togglemode={toggleMode}/>
-     <Alert alert={alert}/>
-     {/* <About className="container my-3"/> */}
-     <div className="container my-3">
-     <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>
-     </div>
-     </>
+            <Router>
+                <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} togglemode={toggleMode} />
+                <Alert alert={alert} />
+                <div className="container my-3">
+                    <Routes>
+                        <Route path="/about" element={<About />} />
+                        <Route
+                            path="/"
+                            element={<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />}
+                        />
+                    </Routes>
+                </div>
+            </Router>
+        </>
   );
 
 }
