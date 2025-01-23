4efc4294a5fd3202f1fe651f8d6f72a17a1758e9
karwasara <pemaharish@gmail.com>
2025-01-23 10:56:02 +0530

    commit

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -4,29 +4,10 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with aharry
-        </a>
-      </header>
-    </div>
-=======
-     <Navbar title="textutils" aboutText="about textutil33"/>
->>>>>>> 064af72f1439f25906a58cb4025f86459c8721c8
-  );
+     
+<Navbar title="textutil" aboutText="about"/>
 
-  
+  );
 }
 
 export default App;
